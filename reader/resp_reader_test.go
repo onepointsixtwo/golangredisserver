b6f782ea27f221dea1e333aa4206803ac6b48fe9
John Kartupelis <john.kartupelis@experian.com>
2018-09-14 13:54:33 +0100

    Add new test to cover extended functionality

--- reader/resp_reader_test.go
+++ reader/resp_reader_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestRespReaderCanReadSetCommandCorrectly(t *testing.T) {
-	reader := mocks.NewMockReader("*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n")
+	reader := mocks.NewMockReader("*4\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n:5\r\n")
 	respRead := CreateRespCommandReader(reader)
 
 	command, err := respRead()
@@ -19,7 +19,8 @@ func TestRespReaderCanReadSetCommandCorrectly(t *testing.T) {
 	commandString := command.Command
 	firstArg := command.Args[0]
 	secondArg := command.Args[1]
-	if commandString != "SET" || firstArg != "mykey" || secondArg != "myvalue" {
-		t.Errorf("Expected command to be SET with args mykey, myvalue but was %v with args %v, %v", commandString, firstArg, secondArg)
+	thirdArg := command.Args[2]
+	if commandString != "SET" || firstArg != "mykey" || secondArg != "myvalue" || thirdArg != "5" {
+		t.Errorf("Expected command to be SET with args mykey, myvalue but was %v with args %v, %v, %v", commandString, firstArg, secondArg, thirdArg)
 	}
 }
